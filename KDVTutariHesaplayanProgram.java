593bfa533f6353d2fbe342c31f3937e7f484842d
Salih Öğüt <79373396+CrincklyRocket@users.noreply.github.com>
2023-06-29 14:53:40 +0300

    Add files via upload

---
+++ KDVTutariHesaplayanProgram.java
@@ -0,0 +1,27 @@
+package Java;
+
+
+import java.util.Scanner;
+
+
+public class KDVTutariHesaplayanProgram {
+    
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        int tutar;
+        double ilkOran = 0.18, ikinciOran = 0.08;
+
+        Scanner inp = new Scanner(System.in);
+        
+        System.out.print("Miktar giriniz =");
+        tutar = inp.nextInt();
+
+        boolean deger = tutar < 1000;
+        String metin = (deger) ? ("KDV oranı ="+ (tutar * ilkOran)) : ("KDV oranı =" + (tutar * ikinciOran));
+       
+        System.out.println(metin);
+    }
+}
